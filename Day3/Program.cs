80c679199727c618af260f85844f7baa758eb9ad
Matthew Duffy <duffym22@gmail.com>
2019-12-02 22:55:33 -0600

    - Added Day3 project

---
+++ Day3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Day3
+{
+    class Program
+    {
+        static string _FILE = "input.csv";
+        static ArrayList read = new ArrayList();
+
+        static void Main(string[] args)
+        {
+            Parse_Input();
+
+            Console.WriteLine("Final value: {0}");
+            Console.ReadLine();
+        }
+
+        static void Parse_Input()
+        {
+            using (StreamReader reader = new StreamReader(Path.GetFullPath(_FILE)))
+            {
+                while (!reader.EndOfStream)
+                {
+                    read.Add(reader.ReadLine());
+                }
+            }
+        }
+    }
+}
