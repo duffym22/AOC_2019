8a8901cb75f348b83a37a052c465b3e08f0ab89e
Matthew Duffy <duffym22@gmail.com>
2019-12-03 22:28:04 -0600

    - Added Day 4 template project

---
+++ Day4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day4
+{
+    class Program
+    {
+        static string _FILE = "input.csv";
+        static ArrayList read = new ArrayList();
+
+        static void Main(string[] args)
+        {
+            Stopwatch tmr = new Stopwatch();
+            tmr.Start();
+
+            Read_Input();
+            Parse_Input();
+
+            Console.WriteLine();
+            Display_Runtime(tmr);
+            Console.ReadLine();
+        }
+
+        private static void Display_Runtime(Stopwatch tmr)
+        {
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "Days", tmr.Elapsed.Days));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "Hours", tmr.Elapsed.Hours));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "Minutes", tmr.Elapsed.Minutes));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "Seconds", tmr.Elapsed.Seconds));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "Milliseconds", tmr.Elapsed.Milliseconds));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "Ticks", tmr.Elapsed.Ticks));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "TotalDays", tmr.Elapsed.TotalDays));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "TotalHours", tmr.Elapsed.TotalHours));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "TotalMinutes", tmr.Elapsed.TotalMinutes));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "TotalSeconds", tmr.Elapsed.TotalSeconds));
+            Console.WriteLine(string.Format("{0,-20}:{1,22}", "TotalMilliseconds", tmr.Elapsed.TotalMilliseconds));
+        }
+
+        static void Read_Input()
+        {
+            using (StreamReader reader = new StreamReader(Path.GetFullPath(_FILE)))
+            {
+                while (!reader.EndOfStream)
+                {
+                    read.Add(reader.ReadLine());
+                }
+            }
+        }
+
+        static void Parse_Input()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
